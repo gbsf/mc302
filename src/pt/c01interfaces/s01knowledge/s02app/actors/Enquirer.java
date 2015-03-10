692f1857a8275d253697e3380c2cdcae77272c3e
Gabriel Souza Franco <gabrielfrancosouza@gmail.com>
2015-03-10 00:02:09 -0300

    Otimizar o número de perguntas feitas

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -12,29 +12,31 @@ import java.util.*;
 public class Enquirer implements IEnquirer {
 
     List<String> respostasPossiveis = new ArrayList<String>();
-    Map<String, Par<Collection<String>, Collection<String>>> perguntas =
-            new HashMap<String, Par<Collection<String>, Collection<String>>>();
+    Map<String, PerguntaPorAnimal> perguntas = new HashMap<String, PerguntaPorAnimal>();
 
     public Enquirer() {
         IBaseConhecimento bc = new BaseConhecimento();
 
         String[] nomes = bc.listaNomes();
 
+        // Analizar todas as perguntas de todos os animais
         for (String nome : nomes) {
             respostasPossiveis.add(nome);
             IObjetoConhecimento obj = bc.recuperaObjeto(nome);
 
             for (IDeclaracao decl = obj.primeira(); decl != null; decl = obj.proxima()) {
-                Par<Collection<String>, Collection<String>> candidatos = perguntas.get(decl.getPropriedade());
-                if (candidatos == null)
-                    candidatos = new Par<Collection<String>, Collection<String>>(
-                        new ArrayList<String>(), new ArrayList<String>());
+                String perguntaString = decl.getPropriedade();
+                PerguntaPorAnimal pergunta = perguntas.get(perguntaString);
+
+                // Criar nova pergunta se não existir
+                if (pergunta == null)
+                    pergunta = new PerguntaPorAnimal(perguntaString);
 
                 if ("sim".equals(decl.getValor()))
-                    candidatos.getPrimeiro().add(nome);
+                    pergunta.getSims().add(nome);
                 else if ("nao".equals(decl.getValor()))
-                    candidatos.getSegundo().add(nome);
-                perguntas.put(decl.getPropriedade(), candidatos);
+                    pergunta.getNaos().add(nome);
+                perguntas.put(perguntaString, pergunta);
             }
         }
     }
@@ -43,23 +45,31 @@ public class Enquirer implements IEnquirer {
     @Override
     public void connect(IResponder responder) {
 
-        for (Map.Entry<String, Par<Collection<String>, Collection<String>>> entry : perguntas.entrySet()) {
+        List<PerguntaPorAnimal> perguntasSorted = new ArrayList<PerguntaPorAnimal>(perguntas.values());
+        // Organizar as perguntas pela ordem definida
+        Collections.sort(perguntasSorted);
+
+        for (PerguntaPorAnimal pergunta : perguntasSorted) {
             // Fazer a pergunta
-            String resposta = responder.ask(entry.getKey());
+            String resposta = responder.ask(pergunta.getPergunta());
 
             // Se a resposta foi "sim", manter como candidatos somente aqueles que têm "sim" como resposta
             if ("sim".equals(resposta))
-                respostasPossiveis.retainAll(entry.getValue().getPrimeiro());
+                respostasPossiveis.retainAll(pergunta.getSims());
             // Idem para "nao"
             else if ("nao".equals(resposta))
-                respostasPossiveis.retainAll(entry.getValue().getSegundo());
+                respostasPossiveis.retainAll(pergunta.getNaos());
 
             // Se sobrou somente uma opção, parar de fazer perguntas
-            if (respostasPossiveis.size() == 1)
+            if (respostasPossiveis.size() <= 1)
                 break;
         }
 
-        // Se, depois de todas as perguntas, ainda tiver mais de um possível, chutar o primeiro
+        // É impossível não ter sobrado nenhuma resposta.
+        assert !respostasPossiveis.isEmpty();
+
+        // Se, depois de todas as perguntas, ainda tiver mais de um possível, chutar o primeiro.
+        // Senão, a lista só terá um elemento de qualquer maneira.
         boolean acertei = responder.finalAnswer(respostasPossiveis.get(0));
 
         if (acertei)
@@ -68,22 +78,59 @@ public class Enquirer implements IEnquirer {
             System.out.println("fuem! fuem! fuem!");
     }
 
-    // Objeto para guardar um par de elementos imutáveis
-    private static class Par<T1, T2> {
-        private final T1 primeiro;
-        private final T2 segundo;
 
-        public Par(T1 primeiro, T2 segundo) {
-            this.primeiro = primeiro;
-            this.segundo = segundo;
+    /**
+     * Classe que guarda uma pergunta, bem como quais animais responderam sim e quais responderam não.
+     */
+    private static class PerguntaPorAnimal implements Comparable<PerguntaPorAnimal> {
+
+        private final String pergunta;
+        private final Collection<String> sims;
+        private final Collection<String> naos;
+
+        public PerguntaPorAnimal(String pergunta) {
+            this.pergunta = pergunta;
+            sims = new ArrayList<String>();
+            naos = new ArrayList<String>();
         }
 
-        public T1 getPrimeiro() {
-            return primeiro;
+        public String getPergunta() {
+            return pergunta;
         }
 
-        public T2 getSegundo() {
-            return segundo;
+        public Collection<String> getSims() {
+            return sims;
+        }
+
+        public Collection<String> getNaos() {
+            return naos;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (!(obj instanceof PerguntaPorAnimal))
+                return false;
+            PerguntaPorAnimal outro = (PerguntaPorAnimal) obj;
+
+            return this.pergunta.equals(outro.pergunta);
+        }
+
+        /**
+         * Compara qual objeto tem a menor lista de animais que responderam à essa pergunta.
+         */
+        @Override
+        public int compareTo(PerguntaPorAnimal o) {
+            int tam = Math.min(sims.size(), naos.size());
+            int tamOutro = Math.min(o.sims.size(), o.naos.size());
+
+            if (tamOutro < tam)
+                return -1;
+            else if (tamOutro > tam)
+                return  1;
+            else
+                return  0;
         }
     }
 
